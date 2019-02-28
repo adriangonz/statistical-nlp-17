7c24f1039d8858c7b932272b5e33838a9e171423
Adrián González Martín <kaseyo23@gmail.com>
2019-02-28 16:29:16 +0000

    add some code to process the dataset so that it looks like PTB

--- src/process.py
+++ src/process.py
@@ -1,4 +1,7 @@
 import re
+import spacy
+
+from spacy.tokens import Token
 
 # A title starts and ends with one or more '='
 # e.g. '= = Gameplay = ='
@@ -17,8 +20,8 @@ def read_wikitext_corpus(file_path):
     Returns
     ---
     iterator
-        Iterator over blocks of text, where each
-        chunk corresponds to one of the documents.
+        Iterator over blocks of text, where each chunk corresponds to one of
+        the documents.
     """
     with open(file_path, 'r') as file:
         line = None
@@ -38,3 +41,76 @@ def read_wikitext_corpus(file_path):
                 # Don't return empty line
                 # from last iteration
                 yield line
+
+
+class PTBTransformer(object):
+    """
+    Pipeline component which processes the input
+    to make it look like the PTB dataset, by:
+
+        - Lowercase all input.
+        - Replace numbers for N.
+        - Remove punctuation.
+
+    The processed output gets stored on a custom `as_ptb` extension.
+    """
+
+    name = "ptb_transformer"
+
+    def __init__(self):
+        """
+        Initialise some custom attributes.
+        """
+        Token.set_extension("as_ptb", default=None)
+
+    def __call__(self, doc):
+        """
+        Transforms a sentence to look like the PTB dataset.
+
+        Parameters
+        ----
+        doc : spacy.Doc
+            Sentence object with some text.
+
+        Returns
+        ----
+        spacy.Doc
+            Document with processed sentences.
+        """
+        for (index, token) in enumerate(doc):
+            if token.is_punct:
+                continue
+
+            # Lowercase
+            token._.as_ptb = token.lower_
+
+            if token.is_digit:
+                # Transform to N
+                token._.as_ptb = u'N'
+
+        return doc
+
+
+def process_wikitext_corpus(file_path):
+    """
+    Processes the wikitext corpus in chunks, using spacy.
+
+    Parameters
+    ----
+    file_path : string
+        Path to the corpus file.
+
+    Returns
+    ---
+    list
+        List of processed sentences.
+    """
+    nlp = spacy.load(
+        'en', disable=['lemmatizer', 'matcher', 'parser', 'tagger', 'ner'])
+    nlp.add_pipe(nlp.create_pipe('sentencizer'), last=True)
+
+    line_iterator = read_wikitext_corpus(file_path)
+    nlp_pipe = nlp.pipe(line_iterator, batch_size=50, n_threads=2)
+    for line in nlp_pipe:
+        import ipdb
+        ipdb.set_trace()
--- src/tests/test_process.py
+++ src/tests/test_process.py
@@ -1,6 +1,24 @@
-from ..process import read_wikitext_corpus
+from ..process import (read_wikitext_corpus, PTBTransformer,
+                       process_wikitext_corpus)
 
 
 def test_read_wikitext_corpus(wiki_file_path):
     lines = list(read_wikitext_corpus(wiki_file_path))
     assert len(lines) == 39
+
+
+def test_ptb_transformer(wiki_doc):
+    transformer = PTBTransformer()
+    transformed_doc = transformer(wiki_doc)
+
+    for token in transformed_doc:
+        if token.is_punct:
+            assert token._.as_ptb is None
+        elif token.is_digit:
+            assert token._.as_ptb == u'N'
+        else:
+            assert token._.as_ptb == token.text.lower()
+
+
+def test_process_wikitext_corpus(wiki_file_path):
+    process_wikitext_corpus(wiki_file_path)
