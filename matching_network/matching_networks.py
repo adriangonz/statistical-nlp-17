1557ba421a87ceb05764eb72f75b030c7e37572a
pilatracu <pilatracu@yahoo.com>
2019-03-08 16:09:37 +0000

    Update matching_networks.py

--- matching_network/matching_networks.py
+++ matching_network/matching_networks.py
@@ -15,51 +15,22 @@ import numpy as np
 import torch.nn.functional as F
 from torch.autograd import Variable
 
-# WON'T NEED THIS
-# def convLayer(in_channels, out_channels, keep_prob=0.0):
-    # """3*3 convolution with padding,ever time call it the output size become half"""
-    # cnn_seq = nn.Sequential(
-        # nn.Conv2d(in_channels, out_channels, 3, 1, 1),
-        # nn.ReLU(True),
-        # nn.BatchNorm2d(out_channels),
-        # nn.MaxPool2d(kernel_size=2, stride=2),
-        # nn.Dropout(keep_prob)
-    # )
-    # return cnn_seq
 
-# THIS IS WHAT GENERATES THE IMAGE EMBEDDINGS.
-# WILL NEED TO CHANGE TO SIMPLE WORD EMBEDDINGS
-# class Classifier(nn.Module):
-    # def __init__(self, layer_size=64, num_channels=1, keep_prob=1.0, image_size=28):
-        # super(Classifier, self).__init__()
-        # """
-        # Build a CNN to produce embeddings
-        # :param layer_size:64(default)
-        # :param num_channels:
-        # :param keep_prob:
-        # :param image_size:
-        # """
-        # self.layer1 = convLayer(num_channels, layer_size, keep_prob)
-        # self.layer2 = convLayer(layer_size, layer_size, keep_prob)
-        # self.layer3 = convLayer(layer_size, layer_size, keep_prob)
-        # self.layer4 = convLayer(layer_size, layer_size, keep_prob)
+         
+class SentenceEncoder(nn.Module):
+    def __init__(self, vocab_length, layer_size=64):
+        self.vocab_length = vocab_length
+        self.layer_size = layer_size
+        # Linear layer 
+        self.layer1 = nn.Linear(len(self.vocab_length), self.layer_size)
 
-        # finalSize = int(math.floor(image_size / (2 * 2 * 2 * 2)))
-        # self.outSize = finalSize * finalSize * layer_size
-
-    # def forward(self, image_input):
-        # """
-        # Use CNN defined above
-        # :param image_input:
-        # :return:
-        # """
-        # x = self.layer1(image_input)
-        # x = self.layer2(x)
-        # x = self.layer3(x)
-        # x = self.layer4(x)
-        # x = x.view(x.size()[0], -1)
-        # return x
-
+    def forward(self, sentence):
+        # Sentence shape= 1 x vocab_length
+        output = self.layer1(sentence)
+        return output
+        
+        
+        
 # SHOULD NOT NEED TO CHANGE
 class AttentionalClassify(nn.Module):
     def __init__(self):
