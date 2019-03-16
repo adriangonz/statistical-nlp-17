f2742731a03d4a6dabe9d9eb3d16c603e67646cf
Adrián González Martín <kaseyo23@gmail.com>
2019-03-16 17:08:06 +0000

    change models to state_dict and add tool to evaluate them

---
+++ bin/test.py
@@ -0,0 +1,69 @@
+"""
+Command to test a stored model against the training set.
+"""
+import os
+import torch
+
+from argparse import ArgumentParser
+
+from sklearn.metrics import accuracy_score
+
+from torch.utils.data import DataLoader
+
+from src.matching_network import MatchingNetwork
+from src.evaluation import predict
+from src.data import read_vocab, read_data_set
+from src.datasets import EpisodesSampler, EpisodesDataset
+from src.utils import extract_model_parameters, get_model_name
+
+BATCH_SIZE = 64
+
+parser = ArgumentParser()
+parser.add_argument(
+    "-v",
+    "--vocab",
+    action="store",
+    dest="vocab",
+    type=str,
+    help="Path to the vocab JSON file")
+parser.add_argument(
+    "-m",
+    "--model",
+    action="store",
+    dest="model",
+    type=str,
+    help="Path to the stored model snapshot")
+parser.add_argument("test_set", help="Path to the test CSV file")
+
+
+def _load_model(model_path):
+    model_file_name = os.path.basename(args.model)
+    distance, embeddings, N, k = extract_model_parameters(model_file_name)
+    model_name = get_model_name(distance, embeddings, N, k)
+    model = MatchingNetwork(model_name, distance_metric=distance)
+    model_state_dict = torch.load(model_path)
+    model.load_state_dict(model_state_dict)
+
+    return model, embeddings, N, k
+
+
+def main(args):
+    print("Loading model...")
+    model, _, N, k = _load_model(args.model)
+
+    print("Loading dataset...")
+    vocab = read_vocab(args.vocab)
+    X_test, y_test = read_data_set(args.test_set, vocab)
+    test_set = EpisodesDataset(X_test, y_test, k=k)
+    sampler = EpisodesSampler(test_set, N=N)
+    test_loader = DataLoader(test_set, sampler=sampler, batch_size=BATCH_SIZE)
+
+    print("Evaluating model...")
+    labels, predictions = predict(model, test_loader)
+    accuracy = accuracy_score(labels, predictions)
+    print(f"ACCURACY = {accuracy:.3f}")
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    main(args)
---
+++ src/evaluation.py
@@ -0,0 +1,52 @@
+"""
+Utilities for evaluating models.
+"""
+
+import torch
+
+
+def predict(model, test_loader):
+    """
+    Predicts the output of an entire test loader.
+
+    Parameters
+    ---
+    model : torch.nn.Module
+        Pre-trained model.
+    test_loader : torch.data.DataLoader
+        DataLoader over the test set.
+
+    Returns
+    ---
+    labels : torch.Tensor[num_episodes]
+        Actual labels for each episode.
+    predictions : torch.Tensor[num_episodes]
+        Predictions for each episode.
+    """
+    num_episodes = len(test_loader.sampler)
+    labels = torch.zeros(num_episodes)
+    predictions = torch.zeros(num_episodes)
+
+    prev_idx = 0
+    for batch_index, batch in enumerate(test_loader):
+        # Make predictions!
+        # The first thre parameters are input for the model
+        batch_predictions = model(batch[:3])
+        # The last one are the correct labels
+        target_labels = batch[-1]
+
+        # Get predicted token and flatten
+        # NOTE: Here we are assuming a single target per episode!!
+        batch_predictions = batch_predictions.argmax(dim=2)
+        batch_predictions = batch_predictions.squeeze()
+        target_labels = target_labels.squeeze()
+
+        # Store predictions and labels on next index
+        batch_size = target_labels.shape[0]
+        next_idx = prev_idx + batch_size
+        labels[prev_idx:next_idx] = target_labels
+        predictions[prev_idx:next_idx] = batch_predictions
+
+        prev_idx = next_idx
+
+    return labels, predictions
