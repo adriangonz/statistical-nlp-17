53f09116753121df0cb9523f7c4af07d847d9336
ad-szwarc <ad.szwarc@gmail.com>
2019-03-17 04:10:52 +0000

    heatmap for attention

---
+++ bin/heatmap.py
@@ -0,0 +1,63 @@
+from argparse import ArgumentParser
+import numpy as np
+import matplotlib.ticker as ticker
+import matplotlib.cm as cm
+import matplotlib as mpl
+import tkinter
+import os
+import matplotlib.pyplot as plt
+
+
+
+parser = ArgumentParser()
+parser.add_argument(
+    "-r",
+    "--attention",
+    action="store",
+    dest="attention",
+    type=str,
+    help="Path to the stored model's attentions")
+
+
+
+def main(args):
+	results = np.load(args.attention)
+	attention = np.array(results['attention'])
+	labels = np.array(results['labels'])
+	support_set = np.array(results['support_set'])
+	target_labels = np.array(results['target_labels'])
+	targets = np.array(results['targets'])
+	print(attention)
+	print(labels)
+	print(support_set)
+	print(target_labels)
+	print(targets)
+	print(attention.shape)
+	print(labels.shape)
+	print(support_set.shape)
+	print(target_labels.shape)
+	print(targets.shape)
+
+
+	data = attention.squeeze(0)
+	fig, axis = plt.subplots() 
+	heatmap = axis.pcolor(data, cmap=plt.cm.Blues) 
+
+	axis.set_yticks([0.5,1.5,2.5,3.5,4.5], minor=False)
+	axis.set_xticks([0.5,1.5,2.5], minor=False)
+	
+	#column_labels = support_set[1]
+	column_labels = [1,2,3]	
+	axis.invert_yaxis()
+	plt.xlabel('Example (k)')
+	plt.ylabel('Label (N)')
+	axis.set_yticklabels(labels, minor=False)
+	axis.set_xticklabels(column_labels, minor=False)
+	plt.title('Attention map for examples in Support Set \n', fontsize=16)
+	fig.set_size_inches(11.03, 7.5)
+	plt.colorbar(heatmap)
+	plt.savefig('_heatmap.png', dpi=100)
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    main(args)
