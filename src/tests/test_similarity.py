6b165298cc09a8ad267c7ed7c3868f6a1e76bdb3
Adrián González Martín <kaseyo23@gmail.com>
2019-03-17 22:42:39 +0000

    fix tests

--- src/tests/test_similarity.py
+++ src/tests/test_similarity.py
@@ -1,21 +1,21 @@
 import torch
 
 from ..similarity import (euclidean_similarity, minkowski_similarity,
-                          cosine_similarity, poincare_similarity, EPS)
+                          cosine_similarity, poincare_similarity)
 
 
 def test_euclidean_similarity(x, y):
     euclidean = euclidean_similarity(x, y)
-    expected = torch.Tensor([0.59359, 1 / EPS])
+    expected = torch.Tensor([-1.6847, -0])
 
-    assert torch.allclose(euclidean, expected)
+    assert torch.allclose(euclidean, expected, rtol=1e-4)
 
 
 def test_minkowski_similarity(x, y):
     minkowski = minkowski_similarity(x, y)
-    expected = torch.Tensor([0.53973, 1 / EPS])
+    expected = torch.Tensor([-1.8528, -0])
 
-    assert torch.allclose(minkowski, expected)
+    assert torch.allclose(minkowski, expected, rtol=1e-4)
 
 
 def test_cosine_similarity(x, y):
